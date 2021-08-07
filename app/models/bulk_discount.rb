8082499d34a62f814c8e52f6aeb04dcdad812ac3
Matt Kragen <mattdkragen@gmail.com>
2021-08-07 14:48:31 -0700

    Add and test validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,3 +1,5 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
+  validates :pct_discount, presence: true, numericality: true
+  validates :qty_threshold, presence: true, numericality: true
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 RSpec.describe BulkDiscount do
   describe 'relationships' do
     it { should belong_to(:merchant) }  
+    it { should validate_presence_of(:pct_discount) }
+    it { should validate_numericality_of(:pct_discount) }
+    it { should validate_presence_of(:qty_threshold) }
+    it { should validate_numericality_of(:qty_threshold) }
   end
 end
