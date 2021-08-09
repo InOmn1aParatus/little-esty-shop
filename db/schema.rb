63135bf80135be1a3a6811974d9a916eb216bdc4
Matt Kragen <mattdkragen@gmail.com>
2021-08-09 14:16:01 -0700

    Perform migration rollback; remove AR associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_191008) do
+ActiveRecord::Schema.define(version: 2021_08_07_040040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,7 @@ ActiveRecord::Schema.define(version: 2021_08_09_191008) do
     t.bigint "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "merchant_id"
     t.index ["customer_id"], name: "index_invoices_on_customer_id"
-    t.index ["merchant_id"], name: "index_invoices_on_merchant_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -85,7 +83,6 @@ ActiveRecord::Schema.define(version: 2021_08_09_191008) do
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
-  add_foreign_key "invoices", "merchants"
   add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
 end
